5bf082bd0eae1f87b5cc8ea1d58bb5a7b86da6bf
makoto0212 <70053303+h-makoto0212@users.noreply.github.com>
2020-09-07 09:19:16 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,176 @@
+import { KintoneManager } from "./kintoneManager.js";
+import { Utilities } from "clasp";
+const getFormResponse = ({ response }) => {
+    const itemResponses = response.getItemResponses(); //アンケートの回答を取得
+    let records = "[{";
+    //回答者のEmailアドレスの取得
+    //メールアドレスの収集設定が有効でなければコメントアウト
+    /*
+        const mailAddress = e.response.getRespondentEmail();
+        const mailFieldCode = "Email";
+        records += `"${mailFieldCode}": { "value": "${mailAddress}" },`;
+    */
+
+    //Mapオブジェクトの定義
+    //[フォームアイテムのタイトル,対応するkintoneのフィールドコード]...
+    /* flags:
+        チェックボックス・複数選択の場合はフィールドコードの末尾に「/m」を付ける
+        作成日時・日時の場合はフィールドコードの末尾に「/d」を付ける
+        ユーザー選択・組織選択・グループ選択の場合はフィールドコードの末尾に「/um」を付ける
+        作成者・更新者の場合はフィールドコードの末尾に「/u」を付ける
+        添付ファイルの場合はフィールドコードの末尾に「/f」を付ける
+    */
+    let cMap = new Map([
+        ["名前", ["name1", "name2"]],
+        ["年齢", ["age1", "age2"]],
+        ["出身地", ["from1", "from2"]],
+        ["代表者名", "leader/um"],
+        ["好きな果物(複数可)", "fav-fruits/m"],
+        ["予約日時", "reservation-date-and-time/d"],
+        ["添付ファイル", "file/f"]
+    ]);
+
+    //配列型のJSON形式対応
+    //in: hoge,fuga,piyo
+    //out: "hoge"."fuga","piyo"
+    const convertToArrayType = (postValues) => {
+        postValues = postValues.map((x) => `"${x}"`);
+        return postValues.join(",");
+    };
+
+    //日時の形式対応
+    //in: yyyy/MM/DD HH:mm:ss
+    //out: yyyy-MM-dd T HH:mm:ss +09:00
+    const convertToDateAndTimeValue = (postValue) => {
+        const timeZone = "Asia/Tokyo";
+        const ISOformat = "yyyy-MM-dd'T'HH:mm:ss'+09:00'";
+        return Utilities.formatDate(
+            new Date(postValue),
+            timeZone,
+            ISOformat
+        );
+    };
+
+    //ユーザー系の形式対応
+    const convertToUserType = (postValue) => {
+        if (postValue.constructor === Array) {
+            postValue = postValue.map((x) => `{ "code" : "${x}" }`);
+            return postValue.join(",");
+        } else {
+            return `{ "code" : "${postValue}"}`;
+        }
+    };
+
+    //添付ファイルの形式対応
+    const convertToFileType = (postValue) => {
+        if (postValue.constructor === Array) {
+            postValue = postValue.map((x) => `{ "fileKey" : "${x}" }`);
+            return postValue.join(",");
+        } else {
+            return `{ "fileKey" : "${postValue}"}`;
+        }
+    };
+
+    const convertToJsonText = (postCode, postValue) => {
+        let isArrayType = false;
+        if (/\/(.*?)d/.test(postCode)) {
+            postValue = convertToDateAndTimeValue(postValue);
+        } else if (/\/(.*?)u/.test(postCode)) {
+            postValue = convertToUserType(postValue);
+            if (/\/(.*?)m/.test(postCode)) {
+                isArrayType = true;
+            }
+        } else if (/\/(.*?)f/.test(postCode)) {
+            isArrayType = true;
+            postValue = convertToFileType(postValue);
+        } else if (/\/(.*?)m/.test(postCode)) {
+            isArrayType = true;
+            postValue = convertToArrayType(postValue);
+        } else {
+            postValue = `"${postValue}"`;
+        }
+        postCode = postCode.replace(/\/(.*)/, ""); //フラグを削除する
+        return isArrayType
+            ? `"${postCode}": { "value" : [${postValue}] },`
+            : `"${postCode}": { "value" : ${postValue} },`;
+    };
+
+    itemResponses.forEach((itemResponse) => {
+        const title = itemResponse.getItem().getTitle();
+        if (cMap.has(title)) {
+            const postValue = itemResponse.getResponse();
+            const postCode = cMap.get(title);
+            if (postCode.constructor === Array) {
+                const postCodeChild = postCode[0];
+                records += convertToJsonText(postCodeChild, postValue);
+                if (postCode.length > 1) {
+                    cMap.set(title, postCode.slice(1));
+                }
+            } else {
+                records += convertToJsonText(postCode, postValue);
+            }
+        }
+    });
+    records = records.slice(0, -1); //末尾のカンマを削除する
+    records += "}]";
+    console.log("Response JSON is '%s'", records);
+    return records;
+};
+const getFormFileDate = ({response},manager) => {
+    const itemResponses = response.getItemResponses(); //アンケートの回答を取得
+    //Mapオブジェクトの定義
+    //[添付ファイルのフォームタイトル,対応するkintoneのフィールドコード]...
+    const fMap = new Map([
+        ["添付ファイル1","file1"],
+        ["添付ファイル2","file2"],
+    ]);
+
+    itemResponses.forEach((itemResponse) => {
+        const title = itemResponse.getItem().getTitle();
+        if (fMap.has(title)) {
+            const postValue = itemResponse.getResponse();
+            const filecode = null;
+        }
+    })
+    
+
+}
+
+const sendToKintone = (e) => {
+    //認証情報を入力する
+    const subdomain = "サブドメイン"; //https://{subdomain}.cybozu.com
+    const apps = {
+        app1: {
+            appid: "アプリID",
+            name: "アプリ名",
+            token: "APIトークン" //パスワード認証の場合は省略可能
+        }
+    };
+
+    //パスワード認証を行う場合はAPIトークンの代わりに以下を追加入力する
+    /*
+        const user = ユーザー名;
+        const pass = パスワード;
+    */
+
+    //ライブラリ「MDT2NQ9jkAGYJ-7ftp_A0v08CaFRWuzzx」を利用する
+    //src: https://github.com/Arahabica/KintoneManager/blob/master/KintoneManager.gs
+    //doc: https://qiita.com/Arahabica/items/063877b0da439020d2c2
+    const manager = new KintoneManager.KintoneManager(subdomain, apps);
+    //パスワード認証の場合は直前の行を下のように書き換える
+    //const manager = new KintoneManager.KintoneManager(subdomain, apps, user, pass);
+    let str = getFormResponse(e);//POSTする通常回答を取得する
+
+    //Googleフォームの添付ファイルを取得する
+    //使わない場合はコメントアウト
+    str += getFormFileDate(e,manager);
+    //特殊文字のエスケープ処理
+    str = str.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t");
+    const records = JSON.parse(str); //JSON形式に変換
+    const response = manager.create("app1", records); //kintone レコードの生成
+    // レスポンス結果
+    const code = response.getResponseCode();
+    const body = response.getContentText();
+    console.log("Response code is '%s'", code); // 成功すれば200になる
+    console.log("Response body is '%s'", body); // エラーならここに詳細が出力される
+};
---
+++ src/kintoneManager.js
@@ -0,0 +1,299 @@
+import { Utilities } from "clasp";
+import { UrlFetchApp } from "clasp";
+import { DriveApp } from "clasp";
+
+
+/**
+ * user, passが指定されれば、パスワード認証
+ * 指定されなければ、APIトークン認証
+ * Basic認証を使用している場合はオプションを有効にする
+ * appsは以下の形式
+ * {
+ *    // アプリケーション名はkintoneのデータに依存せず、GAS内のコードで取り扱う専用
+ *    YOUR_APP_NAME1: {
+ *      appid: 1,
+ *      guestid: 2,
+ *      name: "日報",
+ *      token: "XXXXXXXXXXXXXX_YOUR_TOKEN_XXXXXXXXXXXXXX" // 省略可。APIトークン認証時に使用
+ *    },
+ *    YOUR_APP_NAME2: {
+ *       ...
+ *    }
+ * }
+ * basicは以下の形式(ドメイン共通で指定されている認証情報を入力)
+ * {
+ *   user: "example",
+ *   pass: "xxxxx"
+ * }
+ */
+
+/**
+ * Initialize
+ * @param {string} subdomain your subdomain (For kintone.com domains,
+ *   you must state the FQDN such as "subdomain.kintone.com" )
+ * @param {object} apps list of Applications
+ * @param {object} apps.app application information
+ * @param {number} apps.app.appid application ID
+ * @param {string} apps.app.name application name
+ * @param {number} [apps.app.guestid] Guest id if you are a guest account.
+ * @param {string} [apps.app.token] accessible API tokens ex) "API_TOKEN1,API_TOKEN2"
+ * @param {string} [user] user name or encoded authentication information: base64("USER:PASS")
+ * @param {string} [pass] password
+ * @param {object} [basic] user and pass required for basic authentication
+ * @param {string} [basic.user] authentication fragment
+ * @param {string} [basic.pass] authentication fragment
+ * @constructor
+ */
+class KintoneManager {
+    constructor(subdomain, apps, user, pass, basic) {
+        this.subdomain = subdomain;
+        this.authorization = null;
+        this.apps = apps;
+        this.basic =
+            !!basic && Utilities.base64Encode(`${basic.user}:${basic.pass}`);
+
+        if (arguments.length > 3) {
+            this.authorization = Utilities.base64Encode(`${user}:${pass}`);
+        } else if (arguments.length > 2) {
+            // 引数が3つの場合はエンコード済みの認証情報として処理
+            this.authorization = user;
+        }
+    }
+
+    /**
+     * Records registration
+     * @param {string} app_name Application name
+     * @param {Array} records Kintone record objects ref) https://developer.cybozu.io/hc/ja/articles/201941784
+     * @returns {HTTPResponse} ref) https://developers.google.com/apps-script/reference/url-fetch/http-response
+     */
+    create(app_name, records) {
+        const app = this.apps[app_name];
+        const payload = {
+            app: app.appid,
+            records
+        };
+        const response = UrlFetchApp.fetch(
+            "@1/records.json".replace(/@1/g, this._getEndpoint(app.guestid)),
+            this._postOption(app, payload)
+        );
+        return response;
+    }
+
+    /**
+     * Search records
+     * @param {string} app_name Application name
+     * @param {string} query kintone API query ref) https://developer.cybozu.io/hc/ja/articles/202331474-%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89%E3%81%AE%E5%8F%96%E5%BE%97-GET-#step2
+     * @returns {Array} search results
+     */
+    search(app_name, query) {
+        const q = encodeURIComponent(query);
+        const app = this.apps[app_name];
+        const response = UrlFetchApp.fetch(
+            "@1/records.json?app=@2&query=@3"
+                .replace(/@1/g, this._getEndpoint(app.guestid))
+                .replace(/@2/g, app.appid)
+                .replace(/@3/g, q),
+            this._getOption(app)
+        );
+        return response;
+    }
+
+    /**
+     * Updates records
+     * @param {string} app_name Application name
+     * @param {Array} records Array of records that will be updated.
+     * @returns {HTTPResponse} ref) https://developers.google.com/apps-script/reference/url-fetch/http-response
+     */
+    update(app_name, records) {
+        const app = this.apps[app_name];
+        const payload = {
+            app: app.appid,
+            records
+        };
+        const response = UrlFetchApp.fetch(
+            "@1/records.json".replace(/@1/g, this._getEndpoint(app.guestid)),
+            this._putOption(app, payload)
+        );
+        return response;
+    }
+
+    /**
+     * Deletes Records
+     * @param {string} app_name Application name
+     * @param {Array} record_ids Array of record IDs that will be deleted.
+     * @returns {HTTPResponse} ref) https://developers.google.com/apps-script/reference/url-fetch/http-response
+     */
+    destroy(app_name, record_ids) {
+        const app = this.apps[app_name];
+        const query = record_ids.reduce((prev, current, index) => {
+            return (
+                prev +
+                "&ids[@1]=@2".replace(/@1/g, index).replace(/@2/g, current)
+            );
+        }, `app=${app.appid}`);
+
+        const response = UrlFetchApp.fetch(
+            "@1/records.json?@2"
+                .replace(/@1/g, this._getEndpoint(app.guestid))
+                .replace(/@2/g, query),
+            this._deleteOption(app)
+        );
+        return response;
+    }
+
+    /**
+     * Upload File
+     * @param {string} app_name Application name
+     * @param {string} file_id Upload file of Google Drive fIle ID
+     * @returns {HTTPResponse} ref) https://developer.cybozu.io/hc/ja/articles/201941824
+     */
+    upload(app_name, file_id) {
+        const app = this.apps[app_name];
+        const file = DriveApp.getFileById(file_id);
+        const boundary = "blob";
+        // data -> multipart/form-data
+        const data = `--${boundary}
+  Content-Disposition: form-data; name="file"; filename="${file.getName()}"
+  Content-Type:${file.getMimeType()}\r\n\r\n`;
+        const payload = Utilities.newBlob(data)
+            .getBytes()
+            .concat(file.getBlob().getBytes())
+            .concat(Utilities.newBlob(`\r\n--${boundary}--`).getBytes());
+        const response = UrlFetchApp.fetch(
+            "@1/file.json".replace(/@1/g, this._getEndpoint(app.guestid)),
+            this._uploadOption(app, payload, boundary)
+        );
+        return response;
+    }
+
+    /**
+     * option for GET Method
+     * @param {object} app Application object
+     * @returns {object} Option for UrlFetchApp
+     * @private
+     */
+    _getOption(app) {
+        const option = {
+            method: "get",
+            headers: this._authorizationHeader(app),
+            muteHttpExceptions: true
+        };
+        return option;
+    }
+
+    /**
+     * option for POST Method
+     * @param {object} app Application object
+     * @param {object} payload Request payload
+     * @returns {object} Option for UrlFetchApp
+     * @private
+     */
+    _postOption(app, payload) {
+        const option = {
+            method: "post",
+            contentType: "application/json",
+            headers: this._authorizationHeader(app),
+            muteHttpExceptions: true,
+            payload: JSON.stringify(payload)
+        };
+        return option;
+    }
+
+    /**
+     * option for PUT Method
+     * @param {object} app Application object
+     * @param {object} payload Request payload
+     * @returns {object} Option for UrlFetchApp
+     * @private
+     */
+    _putOption(app, payload) {
+        const option = {
+            method: "put",
+            contentType: "application/json",
+            headers: this._authorizationHeader(app),
+            muteHttpExceptions: true,
+            payload: JSON.stringify(payload)
+        };
+        return option;
+    }
+
+    /**
+     * option for DELETE Method
+     * @param {object} app Application Object
+     * @returns {object} option Option for UrlFetchApp
+     * @private
+     */
+    _deleteOption(app) {
+        const option = {
+            method: "delete",
+            headers: this._authorizationHeader(app),
+            muteHttpExceptions: true
+        };
+        return option;
+    }
+    /**
+     * option for UPLOAD Method
+     * @param {object} app Application Object
+     * @param {object} payload Request payload
+     * @param {string} boundary Character string to identify
+     * @returns {object} option Option for UrlFetchApp
+     * @private
+     */
+    _uploadOption(app, payload, boundary) {
+        const option = {
+            method: "post",
+            contentType: `multipart/form-data; boundary=${boundary}`,
+            headers: this._authorizationHeader(app),
+            payload
+        };
+        return option;
+    }
+
+    /**
+     * Gets Endpoint
+     * @param {string} [guest_id] (optional) Guest id if you are a guest account.
+     * @returns {string} Endpoint url
+     * @private
+     */
+    _getEndpoint(guest_id) {
+        const endpoint =
+            this.subdomain.slice(-4) === ".com"
+                ? `https://${this.subdomain}`
+                : `https://${this.subdomain}.cybozu.com`;
+
+        if (guest_id == null) {
+            return `${endpoint}/k/v1`;
+        } else {
+            return endpoint + "/k/guest/@1/v1".replace(/@1/g, guest_id);
+        }
+    }
+
+    /**
+     * Header Authentication Information
+     * @param {object} app Application object
+     * @param {string} app.token (optional)Application's API token
+     * @returns {object} Authentication Information
+     * @throws {Exception} Authentication is null
+     * @private
+     */
+    _authorizationHeader({ token }) {
+        if (!(this.authorization || token)) {
+            throw new Error("Authentication Failed");
+        }
+
+        const _auth = {};
+        if (this.authorization) {
+            // Password authentication
+            _auth["X-Cybozu-Authorization"] = this.authorization;
+        }
+        if (this.authorization && this.basic) {
+            // Basic authentication
+            _auth["Authorization"] = `Basic ${this.basic}`;
+        }
+        if (token) {
+            // API token authentication
+            _auth["X-Cybozu-API-Token"] = token;
+        }
+        return _auth;
+    }
+}
